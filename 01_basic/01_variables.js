a1b8baad4addd66b72f3ed7e5163cf1d89d62856
Vikram Ashish <73683483+vashish144@users.noreply.github.com>
2024-09-12 09:39:37 +0530

    Add files via upload

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+let userId=123435;
+var emailId="virkam.ashish@capgemini.com";
+const roleNo=10;
+
+topic="javaScript";
+
+/* 
+    Prefer not to use var
+    because of issue in block scop and functional scope
+*/
+console.table([userId,emailId,roleNo,topic]);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,21 @@
+'use strict'; // treat all js code as newer version
+
+alert(3+3) // we are using node js not browser
+
+let age=18;
+let name="Vikram";
+let isLogin=true;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
